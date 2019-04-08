a45e60fc7156a0d619f27c0da51e91886b1012ca
AbbasSep <abbassep3200@gmail.com>
2019-04-09 00:45:09 +0430

    add timer

--- src/app/auction/auctionItem/auctionItem.component.ts
+++ src/app/auction/auctionItem/auctionItem.component.ts
@@ -11,6 +11,7 @@ export class AuctionItemComponent implements OnInit {
   toggleHeart = false;
   showRegisterAuction = false;
   hideRegisterAuction = false;
+  remainedTime;
   @Input() auction: Auction;
   constructor() {
 
@@ -19,6 +20,13 @@ export class AuctionItemComponent implements OnInit {
   ngOnInit() {
     if (this.auction.liked === true) {
       this.toggleHeart = true;
+    }
+    if (this.auction) {
+      this.remainedTime = this.ConvertMS(this.auction.remainedTime);
+      setInterval(() => {
+        this.auction.remainedTime = this.auction.remainedTime - 1000;
+        this.remainedTime = this.ConvertMS(this.auction.remainedTime);
+      }, 1000);
     }
     // $('body').css('{backgroundColor: red}');
   }
@@ -41,5 +49,23 @@ export class AuctionItemComponent implements OnInit {
     eventData.stopPropagation();
   }
 
-
+  ConvertMS(ms) {
+    let day,
+        hour,
+        minute,
+        seconds;
+    seconds = Math.floor(ms / 1000);
+    minute = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    hour = Math.floor(minute / 60);
+    minute = minute % 60;
+    day = Math.floor(hour / 24);
+    hour = hour % 24;
+    return {
+        day: day,
+        hour: hour,
+        minute: minute,
+        seconds: seconds
+    };
+  }
 }
